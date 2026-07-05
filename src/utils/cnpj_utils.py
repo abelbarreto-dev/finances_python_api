53890456ab0020f9f5dbdb0c2fbb1115e169c8d3
Abel Carvalho <abelbcarvalho.dev@gmail.com>
2026-07-05 16:17:44 -0300

    feat: cpf and cnpj utils

---
+++ src/utils/cnpj_utils.py
@@ -0,0 +1,91 @@
+from random import choice
+from re import compile
+
+
+def dict_values(value: str | None) -> int | dict[str, int]:
+    values = {}
+
+    for val in range(10):
+        key = str(val)
+        values[key] = val
+
+    for val in range(65, 91):
+        key = chr(val)
+        values[key] = val - 48
+
+    return values[value] if value is not None else values
+
+
+def weights(last: bool) -> tuple[int]:
+    weight = (5, 4, 3, 2, 9, 8, 7, 6, 5, 4, 3, 2)
+
+    if last:
+        weight = (6, *weight)
+
+    return weight
+
+
+def calc_cnpj(value: str, last: bool) -> str:
+    weight = weights(last)
+
+    sum_cnpj = 0
+    id_weigth = 0
+
+    for dg in value:
+        sum_cnpj += dict_values(dg) * weight[id_weigth]
+        id_weigth += 1
+
+    eleven_mod = sum_cnpj % 11
+
+    return str(0 if eleven_mod < 2 else 11 - eleven_mod)
+
+
+def gen_valid_cnpj(is_alpha: bool = False):
+    values: tuple[str] = tuple(dict_values(None).keys())
+
+    if not is_alpha:
+        values = tuple(i for i in values if i.isnumeric())
+
+    cnpj = "".join(choice(values) for i in range(12))
+    digit = calc_cnpj(cnpj, False)
+    cnpj += digit
+    digit = calc_cnpj(cnpj, True)
+
+    return cnpj + digit
+
+
+def gen_invalid_cnpj(is_alpha: bool = False):
+    values: tuple[str] = tuple(dict_values(None).keys())
+
+    if not is_alpha:
+        values = tuple(i for i in values if i.isnumeric())
+
+    cnpj = "".join(choice(values) for i in range(14))
+
+    if is_cnpj(cnpj):
+        return gen_valid_cnpj(is_alpha)
+
+    return cnpj
+
+
+def validate_cnpj(value: str) -> bool:
+    cnpj = value[0:12]
+
+    digit = calc_cnpj(cnpj, False)
+
+    cnpj += digit
+
+    digit = calc_cnpj(cnpj, True)
+
+    cnpj += digit
+
+    return cnpj == value
+
+
+def is_cnpj(value: str):
+    regex = compile(r"^[0-9A-Z]{14}$")
+
+    if regex.match(value) is None:
+        return False
+
+    return validate_cnpj(value)
---
+++ src/utils/cpf_utils.py
@@ -0,0 +1,61 @@
+from random import choice
+from re import compile
+
+
+def calc_cpf_checker(value: str):
+    len_value = len(value) + 1
+
+    sum_nums = 0
+    id_value = 0
+
+    for i in range(len_value, 1, -1):
+        num = value[id_value]
+        sum_nums += i * int(num)
+        id_value += 1
+
+    eleven_mod = sum_nums % 11
+
+    return str(0 if eleven_mod < 2 else 11 - eleven_mod)
+
+
+def gen_valid_cpf() -> str:
+    numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+    cpf = "".join(choice(numbers) for _ in range(9))
+
+    cpf += calc_cpf_checker(cpf)
+
+    return cpf + calc_cpf_checker(cpf)
+
+
+def gen_invalid_cpf() -> str:
+    numbers = ("0", "1", "2", "3", "4", "5", "6", "7", "8", "9")
+
+    cpf = "".join(choice(numbers) for _ in range(11))
+
+    if is_cpf(cpf):
+        return gen_invalid_cpf()
+
+    return cpf
+
+
+def is_cpf_numeric(value: str) -> bool:
+    regex = compile(r"^[0-9]{11}$")
+    return regex.match(value) is not None
+
+
+def is_cpf(value: str) -> bool:
+    checker = is_cpf_numeric(value)
+
+    cpf = value[0:9]
+
+    if not checker:
+        return False
+
+    digit = calc_cpf_checker(cpf)
+
+    cpf += digit
+
+    digits = digit + calc_cpf_checker(cpf)
+
+    return value[9:11] == digits
